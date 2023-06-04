8f95435c777108bbfef46a6862202f2298615403
Jovan Slavujevic <jotzilla96@gmail.com>
2023-06-04 23:08:21 +0200

    Reading from the Hard Drive & error handling

--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-
-	// fmt.Println(cards.toString())
 	cards.saveToFile("my_cards")
+
+	cardsA := newDeckFromFile("my_cards") // no error (file exists)
+	fmt.Println(cardsA.toString())
+
+	cardsB := newDeckFromFile("my_") // error occures
+	fmt.Println(cardsB.toString())
 
 	// to capture both of the values
 	// hand, remainingDeck := deal(cards, 5)
