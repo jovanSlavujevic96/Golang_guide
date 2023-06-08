c8db4658d54bceb619b9e278f6867c9b2ac25c10
Jovan Slavujevic <jotzilla96@gmail.com>
2023-06-08 23:44:20 +0200

    Created receiver func on copy of the object

--- structs/main.go
+++ structs/main.go
@@ -10,7 +10,17 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo // use one struct within another one
+	// contact   contactInfo // use one struct within another one
+	contactInfo // actually we have field named & type of contactInfo
+}
+
+// receiver func
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
 }
 
 func main() {
@@ -26,11 +36,13 @@ func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Party",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "jim@gmail.com",
 			zipCode: 94000,
 		},
 	}
 
-	fmt.Printf("%+v", jim)
+	jim.updateName("")
+	jim.updateName("jimmy") // this is not going to update the value
+	jim.print()
 }
