c29bcf36d45363dd97c504f4fb7ac96dc62b4ad6
Jovan Slavujevic <jotzilla96@gmail.com>
2023-06-09 00:54:38 +0200

    Iterating over maps

--- maps/main.go
+++ maps/main.go
@@ -6,27 +6,34 @@ func main() {
 	// var colors map[string]string // creates empty map
 
 	// make is a builtin function
-	colors := make(map[string]string) // creates empty map, too
+	// colors := make(map[string]string) // creates empty map, too
 
 	// create map with following types
 	// [string] -> string as a key
 	// string   -> string as a value
-	// colors := map[string]string{  // add some pairs
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
+	colors := map[string]string{ // add some pairs
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
 	// add new pair
-	colors["white"] = "#ffffff"
+	// colors["white"] = "#ffffff"
 
-	fmt.Println(colors)
+	// print map
+	// fmt.Println(colors)
 
-	colorsInts := make(map[int]string)
-	colorsInts[0xffffff] = "white"
-	fmt.Println(colorsInts)
+	// colorsInts := make(map[int]string)
+	// colorsInts[0xffffff] = "white"
 
 	// delete pair from map
-	delete(colors, "white")
-	fmt.Println(colors)
+	// delete(colors, "white")
 
+	printColorMap(colors)
+}
+
+func printColorMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for ", color, " is ", hex)
+	}
 }
