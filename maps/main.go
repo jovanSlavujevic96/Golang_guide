5516a5b0a1b423ce04bd69601a3df7d924ab9117
Jovan Slavujevic <jotzilla96@gmail.com>
2023-06-09 00:31:27 +0200

    Manipulating Maps

--- maps/main.go
+++ maps/main.go
@@ -3,13 +3,30 @@ package main
 import "fmt"
 
 func main() {
+	// var colors map[string]string // creates empty map
+
+	// make is a builtin function
+	colors := make(map[string]string) // creates empty map, too
+
 	// create map with following types
 	// [string] -> string as a key
 	// string   -> string as a value
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-	}
+	// colors := map[string]string{  // add some pairs
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	// add new pair
+	colors["white"] = "#ffffff"
 
 	fmt.Println(colors)
+
+	colorsInts := make(map[int]string)
+	colorsInts[0xffffff] = "white"
+	fmt.Println(colorsInts)
+
+	// delete pair from map
+	delete(colors, "white")
+	fmt.Println(colors)
+
 }
