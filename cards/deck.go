cbe8641db67a7c9c7e099178e10d6660832daa26
jovanSlavujevic96 <jotzilla96@gmail.com>
2026-07-30 17:38:55 +0000

    Refactor file handling to use os package instead of ioutil

--- cards/deck.go
+++ cards/deck.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"math/rand"
 	"os"
 	"strings"
@@ -48,13 +47,13 @@ func (d deck) toString() string {
 }
 
 func (d deck) saveToFile(filename string) error {
-	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
 }
 
 func newDeckFromFile(filename string) deck {
 	// bs - byte slice
 	// err - error object
-	bs, err := ioutil.ReadFile(filename)
+	bs, err := os.ReadFile(filename)
 	if err != nil {
 		// Option #1 - log the error a return a call to newDeck()
 		// Option #2 - log the error and entirely quit the program
