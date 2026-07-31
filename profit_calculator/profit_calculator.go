4c4dc9f127b09015dbb5790ed2b6fb706acecc9f
jovanSlavujevic96 <jotzilla96@gmail.com>
2026-07-31 11:31:27 +0000

    Refactor profit calculator for improved user input handling and financial calculations

--- profit_calculator/profit_calculator.go
+++ profit_calculator/profit_calculator.go
@@ -5,20 +5,25 @@ import "fmt"
 func main() {
 	var revenue, expenses, tax_rate float64
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
+	getUserInfo("Revenue: ", &revenue)
+	getUserInfo("Expenses: ", &expenses)
+	getUserInfo("Tax rate: ", &tax_rate)
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("Tax rate: ")
-	fmt.Scan(&tax_rate)
-
-	ebt := revenue - expenses
-	profit := ebt * (1 - tax_rate/100)
-	ratio := ebt / profit
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, tax_rate)
 
 	fmt.Println("EBT:", ebt)
 	fmt.Println("profit:", profit)
 	fmt.Println("ratio:", ratio)
+}
+
+func getUserInfo(text string, ret *float64) {
+	fmt.Print(text)
+	fmt.Scan(ret)
+}
+
+func calculateFinancials(revenue, expenses, tax_rate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - tax_rate/100)
+	ratio := ebt / profit
+	return ebt, profit, ratio
 }
