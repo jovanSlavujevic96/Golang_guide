cf4c7764840c9fe0f7c8874f87c5e44d60f5b138
jovanSlavujevic96 <jotzilla96@gmail.com>
2026-07-31 14:08:51 +0000

    Enhance profit calculator with input validation and result storage

--- profit_calculator/README.md
+++ profit_calculator/README.md
@@ -5,3 +5,10 @@ CLI tool that:
 - calculates `earnings before tax` (EBT) & `earnings after tax` (profit)
 - caclulates `ratio` (EBT / profit)
 - `outputs` EBT, profit and the ratio
+
+CLI should:
+- validate users input
+  => show error message & exit if invalid input is provided
+    - no negative numbers
+    - not 0
+- store calculated results into a file
--- profit_calculator/profit_calculator.go
+++ profit_calculator/profit_calculator.go
@@ -1,24 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
 	var revenue, expenses, tax_rate float64
+	var e error
 
-	getUserInfo("Revenue: ", &revenue)
-	getUserInfo("Expenses: ", &expenses)
-	getUserInfo("Tax rate: ", &tax_rate)
+	revenue, e = getUserInfo("Revenue: ")
+	if e != nil {
+		fmt.Println(e.Error())
+		os.Exit(1)
+	}
+	expenses, e = getUserInfo("Expenses: ")
+	if e != nil {
+		fmt.Println(e.Error())
+		os.Exit(1)
+	}
+	tax_rate, e = getUserInfo("Tax rate: ")
+	if e != nil {
+		fmt.Println(e.Error())
+		os.Exit(1)
+	}
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, tax_rate)
 
 	fmt.Println("EBT:", ebt)
 	fmt.Println("profit:", profit)
 	fmt.Println("ratio:", ratio)
+	storeResults(ebt, profit, ratio)
 }
 
-func getUserInfo(text string, ret *float64) {
+func storeResults(ebt, profit, ratio float64) {
+	resutls := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(resutls), 0644)
+}
+
+func isInputValid(input float64) bool {
+	// no negative numbers
+	// no zero value
+	// therefore input must be greater than zero
+	return (input > 0.0)
+}
+
+func getUserInfo(text string) (float64, error) {
 	fmt.Print(text)
-	fmt.Scan(ret)
+
+	var ret float64
+	fmt.Scan(&ret)
+
+	if isInputValid(ret) {
+		return ret, nil
+	}
+	return ret, errors.New("Invalid value for " + text + strconv.FormatFloat(ret, 'f', -1, 64))
 }
 
 func calculateFinancials(revenue, expenses, tax_rate float64) (float64, float64, float64) {
