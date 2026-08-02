9f03e42b9e4da5ba8d77ede664649ecf3a07cd45
jovanSlavujevic96 <jotzilla96@gmail.com>
2026-08-02 22:38:28 +0000

    Refactor FileManager to encapsulate file operations and update TaxIncludedPriceJob to use FileManager for reading and writing files

--- price_calculator/filemanager/filemanager.go
+++ price_calculator/filemanager/filemanager.go
@@ -7,8 +7,13 @@ import (
 	"os"
 )
 
-func ReadLines(path string) ([]string, error) {
-	file, err := os.Open(path)
+type FileManager struct {
+	InputFilePath  string
+	OutputFilePath string
+}
+
+func (fm FileManager) ReadLines() ([]string, error) {
+	file, err := os.Open(fm.InputFilePath)
 
 	if err != nil {
 		return nil, errors.New("Failed to open file.")
@@ -31,8 +36,8 @@ func ReadLines(path string) ([]string, error) {
 	return lines, nil
 }
 
-func WriteJSON(path string, data interface{}) error {
-	file, err := os.Create(path)
+func (fm FileManager) WriteJSON(data interface{}) error {
+	file, err := os.Create(fm.OutputFilePath)
 	if err != nil {
 		return errors.New("Failed to create file.")
 	}
@@ -44,4 +49,11 @@ func WriteJSON(path string, data interface{}) error {
 		return errors.New("Failed to convert data to JSON.")
 	}
 	return nil
+}
+
+func New(inputPath, outputPath string) FileManager {
+	return FileManager{
+		InputFilePath:  inputPath,
+		OutputFilePath: outputPath,
+	}
 }
--- price_calculator/prices/prices.go
+++ price_calculator/prices/prices.go
@@ -8,13 +8,14 @@ import (
 )
 
 type TaxIncludedPriceJob struct {
+	IOManager         filemanager.FileManager
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]string
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
-	lines, err := filemanager.ReadLines("prices.txt")
+	lines, err := job.IOManager.ReadLines()
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -41,7 +42,7 @@ func (job *TaxIncludedPriceJob) Process() {
 
 	job.TaxIncludedPrices = result
 
-	err := filemanager.WriteJSON(fmt.Sprintf("result_%.0f.json", job.TaxRate*100), job)
+	err := job.IOManager.WriteJSON(job)
 	if err != nil {
 		fmt.Println("Writing to JSON failed.")
 		fmt.Println(err)
@@ -49,8 +50,9 @@ func (job *TaxIncludedPriceJob) Process() {
 	}
 }
 
-func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+func NewTaxIncludedPriceJob(fm filemanager.FileManager, taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
+		IOManager:   fm,
 		InputPrices: []float64{10, 20, 30}, // hardcoded ATM
 		TaxRate:     taxRate,
 	}
